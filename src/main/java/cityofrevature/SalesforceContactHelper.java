a38577671e3edc1d8ae45222f8bb9475f3532b02
joshparkerj <superfakek@gmail.com>
2019-06-01 16:54:59 -0500

    fail to invoke upsert

--- src/main/java/cityofrevature/SalesforceContactHelper.java
+++ src/main/java/cityofrevature/SalesforceContactHelper.java
@@ -1,7 +1,9 @@
 package cityofrevature;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,15 +16,36 @@ import com.sforce.soap.enterprise.sobject.Contact;
 
 public class SalesforceContactHelper implements Callable {
 
+	@Override
+	public List<Contact> onCall(MuleEventContext eventContext) throws Exception {
+		BufferInputStream payload = (BufferInputStream) eventContext.getMessage().getPayload();
+		String stringpayload = convertStreamToString(payload);
+		System.out.println(stringpayload);
+		String first = getFirst(stringpayload);
+		String last = getLast(stringpayload);
+		String email = getEmail(stringpayload);
+		System.out.println(first);
+		System.out.println(last);
+		System.out.println(email);
+		DomainContact dc = new DomainContact();
+		dc.setFirst(first);
+		dc.setLast(last);
+		dc.setEmail(email);
+		List<Contact> lc = new ArrayList<Contact>();
+		lc.add(contactFromDomainContact(dc));
+		return lc;
+	}
+
 	private Contact contactFromDomainContact(DomainContact a) {
 		GregorianCalendar gc = new GregorianCalendar();
-		gc.setTime(a.getBirthdate());
+		if (a.getBirthdate() != null)
+			gc.setTime(a.getBirthdate());
 		Contact newContact = new Contact();
 		newContact.setAccountId(a.getAccountid());
 		newContact.setBirthdate(gc);
 		newContact.setEmail(a.getEmail());
 		newContact.setFirstName(a.getFirst());
-		newContact.setId(a.getId());
+		newContact.setId("");
 		newContact.setLastName(a.getLast());
 		newContact.setPhone(a.getPhone());
 		return newContact;
@@ -34,26 +57,16 @@ public class SalesforceContactHelper implements Callable {
 		}
 	}
 
-	@Override
-	public Object onCall(MuleEventContext eventContext) throws Exception {
-		BufferInputStream payload = (BufferInputStream) eventContext.getMessage().getPayload();
-		String stringpayload = convertStreamToString(payload);
-		System.out.println(stringpayload);
-		String first = getFirst(stringpayload);
-		String last = getLast(stringpayload);
-		System.out.println(first);
-		System.out.println(last);
-		DomainContact dc = new DomainContact();
-		// dc.setFirst(payload.getFirst());
-		return contactFromDomainContact(dc);
-	}
-
 	private String getFirst(String input) {
-		return getNamedMatchGroups(input, "\"first\": \"(?<first>[^\"]*)", "first");
+		return getNamedMatchGroups(input, "\"first\": *\"(?<first>[^\"]*)", "first");
 	}
 
 	private String getLast(String input) {
-		return getNamedMatchGroups(input, "\"last\": \"(?<last>[^\"]*)", "last");
+		return getNamedMatchGroups(input, "\"last\": *\"(?<last>[^\"]*)", "last");
+	}
+	
+	private String getEmail(String input) {
+		return getNamedMatchGroups(input, "\"email\": *\"(?<email>[^\"]*)", "email");
 	}
 
 	private String getNamedMatchGroups(String input, String pattern, String name) {
